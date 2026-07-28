ed3ce36253e43fff28ded8a88441f733e59e8d24
Jeffrey Wang <jeffwang125@gmail.com>
2026-07-27 22:40:01 -0400

    SearchServiceTest

--- server/src/test/kotlin/com/jeffreyalanwang/dutchrailways/backend/server/processing/search/SearchServiceTest.kt
+++ server/src/test/kotlin/com/jeffreyalanwang/dutchrailways/backend/server/processing/search/SearchServiceTest.kt
@@ -1,11 +1,37 @@
 package com.jeffreyalanwang.dutchrailways.backend.server.processing.search
 
+import com.jeffreyalanwang.dutchrailways.backend.server.repository.entity.PassService
+import jakarta.transaction.Transactional
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertIs
 
+@SpringBootTest
 class SearchServiceTest(
-    @Autowired searchService: SearchService,
+    @Autowired private val searchService: SearchService,
 ) {
+    @Transactional
     @Test
-    fun test(): Unit = TODO()
+    fun `Query for PassService with anyLike`(): Unit = runBlocking {
+        // Wait for initial indexing to complete
+        searchService.initJob.join()
+
+        val results = searchService.search(
+            anyLike = "Intercity",
+            nameLike = null,
+            near = null,
+            types = listOf(PassService::class),
+            batchSize = 10,
+        ).toList().run {
+            map { assertIs<PassService>(it); it }
+        }
+
+        assertTrue(results.size > 1)
+        assertContains(results.first().name, "Intercity")
+    }
 }
