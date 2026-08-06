9ed1d1df4091582af0b746258031f732038b7a8f
Jeffrey Wang <jeffwang125@gmail.com>
2026-08-06 14:40:34 -0400

    Align JVM toolchains

--- routeQuery/build.gradle.kts
+++ routeQuery/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
 group = "com.jeffreyalanwang.dutchrailways.backend"
 description = "Implementation of transit route planning algorithms"
 
+kotlin {
+    compilerOptions {
+        jvmToolchain(21)
+    }
+}
+
 dependencies {
     testImplementation(libs.kotlinx.datetime)
     testImplementation(libs.bundles.test.junit5)
