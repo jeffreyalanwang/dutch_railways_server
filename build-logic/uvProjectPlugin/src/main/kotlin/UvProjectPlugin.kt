561787831d8c6a18894be69b761afbf7e12fca74
Jeffrey Wang <jeffwang125@gmail.com>
2026-08-11 13:25:54 -0400

    Add source set for uv project automatically

--- build-logic/uvProjectPlugin/src/main/kotlin/UvProjectPlugin.kt
+++ build-logic/uvProjectPlugin/src/main/kotlin/UvProjectPlugin.kt
@@ -1,8 +1,14 @@
+import com.pswidersk.gradle.python.uv.UvPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.plugins.JavaBasePlugin
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 
@@ -10,19 +16,24 @@ class UvProjectPlugin : Plugin<Project> {
 
     abstract class Extension {
         abstract val uvProjectDir: DirectoryProperty
+        internal operator fun component1() = uvProjectDir
     }
 
     override fun apply(target: Project): Unit = target.run {
-        plugins.apply("com.pswidersk.python-uv-plugin")
+        plugins.apply(UvPlugin::class)
+        plugins.apply(JavaBasePlugin::class)
 
-        val extension = extensions.create<Extension>("uvProject").apply {
+        val (uvProjectDirValue) = extensions.create<Extension>("uvProject").apply {
             uvProjectDir.convention(layout.projectDirectory)
+        }
+        afterEvaluate {
+            extensions.getByType<SourceSetContainer>().create(uvProjectDirValue.name.get())
         }
 
         val syncTask = tasks.register<UvSyncTask>("syncUvProject")
 
         tasks.withType<UvProjectTask>().configureEach {
-            uvProjectDir = extension.uvProjectDir
+            uvProjectDir = uvProjectDirValue
         }
         afterEvaluate {
             tasks.withType<UvProjectTask>().configureEach {
@@ -32,3 +43,5 @@ class UvProjectPlugin : Plugin<Project> {
     }
 
 }
+
+private val DirectoryProperty.name get() = map { it.asFile.name }
