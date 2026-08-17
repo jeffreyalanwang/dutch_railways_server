b89da3d1d414c90eaa8bf8c736991399ac3229e5
Jeffrey Wang <jeffwang125@gmail.com>
2026-08-16 21:12:45 -0400

    Add Docker compose tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 }
 
 group = "com.jeffreyalanwang.dutchrailways.backend"
+version = "0.0.1"
 
 subprojects {
     tasks.withType<Test> {
@@ -13,3 +14,20 @@ subprojects {
     }
 }
 
+tasks.register<Exec>("publishDockerCompose") {
+    description = "Publish to Docker Compose"
+    commandLine(
+        "docker", "compose", "publish",
+        "jeffreyalanwang/dutch-railways:$version",
+    )
+}
+
+tasks.register<Exec>("fetchAndRunDockerCompose") {
+    description = "Run Docker Compose from registry"
+    commandLine(
+        "docker", "compose",
+        "-f",
+            "oci://docker.io/jeffreyalanwang/dutch-railways:$version",
+        "up",
+    )
+}
